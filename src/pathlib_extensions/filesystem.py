58b7738922d4d731dc30cf0e285d4ab407970034
Libo Yin <liboyin830@gmail.com>
2025-01-04 02:44:53 +0000

    Added a feature to truncate filenames to fit in 255 bytes

--- src/pathlib_extensions/filesystem.py
+++ src/pathlib_extensions/filesystem.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 import re
 
-__all__ = ['replace_os_reserved_chars']
+__all__ = ['replace_os_reserved_chars', 'truncate_filename']
 
 
 def replace_os_reserved_chars(text: str, replacement: str = '_') -> str:
@@ -17,3 +18,32 @@ def replace_os_reserved_chars(text: str, replacement: str = '_') -> str:
         str: The modified text with reserved characters replaced by `replacement`.
     """
     return re.sub(r'[\\/*?:"<>|]', replacement, text)
+
+
+def truncate_filename(path: str | Path, max_length: int = 255) -> Path:
+    """
+    Truncate the filename such that it fits in `max_length` bytes.
+
+    Args:
+        path (str | Path): The path to process.
+        max_length (int, optional): The maximum number of bytes. Defaults to 255 for ext4.
+
+    Returns:
+        Path: The processed path.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    filename = path.name
+    if len(filename.encode('utf-8')) <= max_length:
+        return path
+    suffixes = ''.join(path.suffixes)
+    suffixes_bytes_length = len(suffixes.encode('utf-8'))
+    if suffixes_bytes_length >= max_length:
+        raise ValueError(f'Not possible to truncate filename to fit in {max_length} bytes: {path}')
+    stem_max_bytes_length = max_length - suffixes_bytes_length
+    stem = filename[:-len(suffixes)]
+    while len(stem.encode('utf-8')) > stem_max_bytes_length:
+        stem = stem[:-1]
+    result = path.parent / (stem + suffixes)
+    print(f'Truncated path: {path} -> {result}')
+    return result
--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,4 +1,8 @@
-from pathlib_extensions.filesystem import replace_os_reserved_chars
+from pathlib import Path
+
+import pytest
+
+from pathlib_extensions.filesystem import replace_os_reserved_chars, truncate_filename
 
 
 def test_remove_os_reserved_chars():
@@ -11,3 +15,34 @@ def test_remove_os_reserved_chars():
     assert replace_os_reserved_chars("file/name") == "file_name"
     assert replace_os_reserved_chars("file\\name") == "file_name"
     assert replace_os_reserved_chars('file"name') == "file_name"
+
+
+def test_truncate_filename_no_change():
+    path = Path("short_filename.txt")
+    result = truncate_filename(path)
+    assert result == path
+
+
+def test_truncate_filename_with_extensions():
+    suffixes = ".tar.gz"
+    result = truncate_filename("a" * 260 + suffixes)
+    assert result == Path("a" * (255 - len(suffixes)) + suffixes), len(str(result))
+
+
+def test_truncate_filename_extension_too_long():
+    path = Path("file." + "a" * 254)
+    with pytest.raises(ValueError, match="Not possible to truncate"):
+        truncate_filename(path)
+
+
+def test_truncate_filename_with_long_directory():
+    dir_path = Path("/" + "a" * 200) / ("b" * 200)
+    result = truncate_filename(dir_path / ("c" * 260 + ".txt"))
+    assert result == dir_path / ("c" * 251 + ".txt"), len(str(result))
+
+
+def test_truncate_filename_with_unicode():
+    result = truncate_filename(Path("🌟" * 100 + ".txt"))
+    assert result.suffixes == [".txt"]
+    result_bytes_length = len(str(result).encode('utf-8'))
+    assert 250 <= result_bytes_length <= 255, result_bytes_length
