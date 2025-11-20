3f39af6199d558036ac19e6d4eb8fa8a79612a46
Libo Yin <liboyin830@gmail.com>
2025-11-20 02:52:38 +0000

    Updated installation logic for AI coding assists

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 # deployment dependencies are installed as root, but current user is vscode
 pip --disable-pip-version-check --no-cache-dir install -e .[dev]
-# install nodeJS for AI code assistants
+# use native installer of Claude Code
+curl -fsSL https://claude.ai/install.sh | bash
+# install nodeJS + Gemini CLI
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends nodejs npm
-# npx @google/gemini-cli
-# npx @anthropic-ai/claude-code
+curl -fsSL https://deb.nodesource.com/setup_25.x | sudo -E bash -
+sudo apt-get install -y --no-install-recommends nodejs
+sudo npm install -g @google/gemini-cli
